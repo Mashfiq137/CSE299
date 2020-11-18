f0fb8424ab44f3cabcbb2f52652f3a6f642f21b9
Niloy Rahman <havocisgod@gmail.com>
2020-11-18 21:18:36 +0600

    adding frament

--- NewsFlash2/app/src/main/java/com/example/newsflash/loginActivity.java
+++ NewsFlash2/app/src/main/java/com/example/newsflash/loginActivity.java
@@ -1,14 +1,45 @@
 package com.example.newsflash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
 public class loginActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FloatingActionButton google;
+    float v = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tabLayout = findViewById(R.id.tab_layout_login);
+        viewPager = findViewById(R.id.viewpager_login);
+        google = findViewById(R.id.sign_in_button);
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(),this, tabLayout.getTabCount())
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        //button transitions
+        google.setTranslationY(300);
+        google.setAlpha(v);
+
+        tabLayout.setTranslationY(300);
+        tabLayout.setAlpha(v);
+
+
+        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+
     }
 }
