b668f783fbcbcdd0fe23530e03db73f9bb5bedec
Niloy Rahman <havocisgod@gmail.com>
2020-11-20 18:06:30 +0600

    updated

--- NewsFlash2/app/src/main/java/com/example/newsflash/LoginTabFragment.java
+++ NewsFlash2/app/src/main/java/com/example/newsflash/LoginTabFragment.java
@@ -5,12 +5,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import androidx.fragment.app.Fragment;
+;import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 public class LoginTabFragment extends Fragment {
+
+    TabLayout tabLayout;
+    FloatingActionButton google;
+    float v = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment, container, false);
+        tabLayout = root.findViewById(R.id.tab_layout_login);
+        google = root.findViewById(R.id.sign_in_button);
+        google.setTranslationY(300);
+        google.setAlpha(v);
+
+        tabLayout.setTranslationY(300);
+        tabLayout.setAlpha(v);
+
+
+        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+
+
+
+
         return root;
     }
 }
--- NewsFlash2/app/src/main/java/com/example/newsflash/MainActivity.java
+++ NewsFlash2/app/src/main/java/com/example/newsflash/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.newsflash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab_login_page);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, loginActivity.class)
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
